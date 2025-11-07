a0756dfebfaac55958e50c4d19763edb7ecf30f4
Jaspher <69jasphergayo@gmail.com>
2025-11-07 17:28:48 +0800

    popo

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body>
   /* Navigation Bar */
   kita mo to? try
-  ashydiuasyhidoujasd
+  angelo
   <header>
     <div class="logo">
       <img src="images/logo.png" alt="School Logo" style="height: 40px;">
